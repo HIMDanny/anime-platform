9e5a9dc92de30492b66288f6638a03114515eaae
HIMDanny <danilbliz0201@gmail.com>
2023-02-17 00:11:19 +0200

    feat: start working on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,42 @@
+import { useLoaderData } from 'react-router-dom';
+import fetchTrendingAnimes from '../api/fetchTrendingAnimes';
+import AnimeCard from '../components/AnimeCard';
+import Anime from '../types/Anime';
+
+type TrendingAnimes = {
+  trendingAnimes: Anime[];
+};
+
+export const loader = async (): Promise<TrendingAnimes> => {
+  const { data } = await fetchTrendingAnimes();
+
+  return { trendingAnimes: data };
+};
+
 const Home = () => {
-  return <div>Home</div>;
+  const { trendingAnimes } = useLoaderData() as TrendingAnimes;
+  console.log(trendingAnimes);
+
+  return (
+    <div className="flex gap-4">
+      {trendingAnimes
+        .slice(0, 3)
+        .map(
+          ({
+            id,
+            attributes: { titles, posterImage, subtype, description },
+          }: Anime) => (
+            <AnimeCard
+              key={id}
+              id={id}
+              title={titles.en ?? titles.en_jp}
+              thumbnail={posterImage.medium}
+              type={subtype}
+              description={description}
+            />
+          ),
+        )}
+    </div>
+  );
 };
 export default Home;
