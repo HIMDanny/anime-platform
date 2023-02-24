93f11e484b5e8e2b5851ec2c6fc87c0781c78392
HIMDanny <danilbliz0201@gmail.com>
2023-02-24 22:33:00 +0200

    chore: delete debounce function
    Function was not useful

--- src/utils/debounce.ts
+++
@@ -1,9 +0,0 @@
-const debounce = (fn: Function, timeDelay = 1000) => {
-  let fnTimer: number | undefined;
-  return (...args: any[]) => {
-    clearTimeout(fnTimer);
-    fnTimer = setTimeout(() => fn(...args), timeDelay);
-  };
-};
-
-export default debounce;
